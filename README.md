6d748ad4b489c6e1c52936771564cc0336b1a5d4
Rohit Nimkar <nehalnimkar@gmail.com>
2022-08-21 22:00:29 +0530

    📝 update documentation

---
+++ README.md
@@ -0,0 +1,41 @@
+# zephyr-switch-interrupt
+
+Compile zephyr rtos project for blue pill board which toggles an led connected to pin C13 on push of button.
+
+![Build Passing](https://img.shields.io/badge/build-passing-brightgreen)
+[![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://opensource.org/licenses/)
+## Run Locally
+
+Clone the project
+1. Using https
+```bash
+  git clone https://github.com/csrohit/zephyr-switch-interrupt
+```
+2. Using ssh
+```bash
+  git clone git@github.com:csrohit/zephyr-switch-interrupt.git
+```
+
+Go to the project directory
+
+```bash
+  cd zephyr-switch-interrupt
+```
+
+Compile Project
+
+Setup zephyr environment on your machine from [Zephyr: Getting Started](https://docs.zephyrproject.org/latest/develop/getting_started/index.html)
+```bash
+  make all
+```
+
+Flash Binary
+
+To flash you need to have
+1. Openocd installed on your machine. You can do it from here: [Getting Openocd](https://openocd.org/pages/getting-openocd.html)
+2. Connect Stlink to PC and blue pill board using swd headers.
+3. Put blue pill board in programming mode
+```bash
+  make flash
+```
+* or you can flash the hex file using [STM32 Cube Programmer](https://docs.nanoframework.net/content/stm32/flash-cube-programmer.html)
--- src/main.c
+++ src/main.c
@@ -1,28 +1,41 @@
 #include <zephyr/zephyr.h>
+#include <zephyr/sys/printk.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/device.h>
 #include <zephyr/devicetree/gpio.h>
 
-#define LED DT_NODELABEL(led)
-static struct gpio_dt_spec spec = GPIO_DT_SPEC_GET(LED, gpios);
-static struct gpio_dt_spec button = GPIO_DT_SPEC_GET(DT_ALIAS(button), gpios);
+
+static struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED, gpios);
+static struct gpio_dt_spec button = GPIO_DT_SPEC_GET(DT_ALIAS(button1), gpios);
 
 static struct gpio_callback button_cb;
+static gpio_callback_handler_t button_pressed(struct device *dev, struct gpio_callback *cb, uint32_t pins);
 
-static gpio_callback_handler_t button_pressed(struct device *dev,
-                         struct gpio_callback *cb,
-                         uint32_t pins)
-{
-  gpio_pin_toggle_dt(&spec);
-}
+
 void main(void)
 {
     int ret;
+
+    // check if led is ready
+    if (!device_is_ready(led.port))
+    {
+        return;
+    }
+
+    // configure led gpio pin as output and set initial value as Logical Active
+    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    if (ret < 0)
+    {
+        return;
+    }
+
+    // check if the button is ready
     if (!device_is_ready(button.port))
     {
         return;
     }
 
+    // configure button gpio as input
     ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
     if (ret != 0)
     {
@@ -31,36 +44,31 @@ void main(void)
         return;
     }
 
-    if (!device_is_ready(spec.port))
-    {
-        return;
-    }
-
-    ret = gpio_pin_configure_dt(&spec, GPIO_OUTPUT_ACTIVE);
-    if (ret < 0)
-    {
-        return;
-    }
-
-    ret = gpio_pin_interrupt_configure_dt(&button,
-					      GPIO_INT_EDGE_RISING);
+    // enable interrupt on button for rising edge
+    ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_RISING);
 	if (ret != 0) {
 		printk("Error %d: failed to configure interrupt on %s pin %d\n",
 			ret, button.port->name, button.pin);
 		return;
 	}
 
-
+    // initialize callback structure for button interrupt
     gpio_init_callback(&button_cb, button_pressed, BIT(button.pin));
 
+    // attach callback function to button interrupt
     gpio_add_callback(button.port, &button_cb);
+}
 
 
-
-
-    // while (1)
-    // {
-    //     ret = gpio_pin_toggle_dt(&spec);
-    //     k_msleep(1000);
-    // }
+/**
+ * @brief interrupt handler for button pressed rising edge
+ * 
+ * @param dev pointer to button 
+ * @param cb pointer to owner gpio_callback
+ * @param pins mask of pins that trigger this interrupt
+ * @return gpio_callback_handler_t 
+ */
+static gpio_callback_handler_t button_pressed(struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+  gpio_pin_toggle_dt(&led);
 }
